f9cd9d6fbfc0fd839258c8d5b9e0927052dd25fa
Grand Nabil <akambilawani@gmail.com>
2023-08-29 07:48:31 +0100

    models for dog details

--- dog_shelters/urls.py
+++ dog_shelters/urls.py
@@ -2,9 +2,12 @@
 
 from django.urls import path
 from . import views
+# TODO: Register detail view
+
 
 urlpatterns = [
     path('', views.shelter_list, name='shelter_list'), # ('') chemin par défaut
     path('shelter/<int:pk>', views.shelter_detail, name='shelter_detail'),
+    path('dog/<int:pk>', views.DogDetailView.as_view(), name='dog_detail'), #enregistrement de la vue details
     # More patterns to come later
 ]
