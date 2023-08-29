36d95e88ed3043fd830a275aba53a1d73f799def
Grand Nabil <akambilawani@gmail.com>
2023-08-29 07:42:56 +0100

    import of generic views

--- dog_shelters/views.py
+++ dog_shelters/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from . import models #importation des gabarits
+# TODO: Import generic views
+from django.views import generic
 
 # charger tous les refuges
 # créez l’objet de contexte pour le gabarit 
@@ -16,3 +18,10 @@ def shelter_detail(request, pk):
     shelter = get_object_or_404(models.Shelter, pk=pk)
     context = {'shelter': shelter}
     return render(request, 'shelter_detail.html', context)
+
+#création de la vue générique pour DogDetail
+# définir le modèle, le template et l’objet de contexte.
+class DogDetailView(generic.DetailView):
+    model = models.Dog
+    template_name = 'dog_detail.html'
+    context_object_name = 'dog'
